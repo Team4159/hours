9268a5cd6b727a3b2e517841d218c7a1b4e489dc
Kai Chang <kaijchang@gmail.com>
2020-09-19 10:36:37 -0700

    remove getUsers endpoint

--- src/pages/api/getUsers.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiResponse } from 'next';
-
-import { getUsers } from '@/utils/dynamodb';
-
-export default (_, res: NextApiResponse) => {
-  getUsers()
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).end(err.toString()));
-};
