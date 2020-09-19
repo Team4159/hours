a5f29231fa9da929a19161c25aab6ca376e1fdd5
Kai Chang <kaijchang@gmail.com>
2020-09-18 21:21:07 -0700

    typing

--- src/pages/api/unflagSession.ts
+++ src/pages/api/unflagSession.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { unflagSession } from '@/utils/dynamodb';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  unflagSession(req.query.password, req.query.sessionEnd)
+  unflagSession(req.query.password as string, req.query.sessionEnd as string)
     .then(() => res.status(200).end('Success'))
     .catch(err => res.status(400).end(err.toString()));
 };
